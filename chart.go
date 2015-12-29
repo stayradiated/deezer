824b00f0b32f0a182133e8697ce90f4ea1e4aa19
George Czabania <george.czabania@sparkventures.co.nz>
2015-12-29 13:08:24 +1300

    Use listParams with chart methods

--- chart.go
+++ chart.go
@@ -19,27 +19,27 @@ func GetCharts(index, limit int) (Chart, error) {
 func GetChartTracks(id, index, limit int) (TrackList, error) {
 	path := fmt.Sprintf("/chart/%d/tracks", id)
 	result := TrackList{}
-	err := get(path, nil, &result)
+	err := get(path, listParams(index, limit), &result)
 	return result, err
 }
 
 func GetChartAlbums(id, index, limit int) (AlbumList, error) {
 	path := fmt.Sprintf("/chart/%d/albums", id)
 	result := AlbumList{}
-	err := get(path, nil, &result)
+	err := get(path, listParams(index, limit), &result)
 	return result, err
 }
 
 func GetChartArtists(id, index, limit int) (ArtistList, error) {
 	path := fmt.Sprintf("/chart/%d/artists", id)
 	result := ArtistList{}
-	err := get(path, nil, &result)
+	err := get(path, listParams(index, limit), &result)
 	return result, err
 }
 
 func GetChartPlaylists(id, index, limit int) (PlaylistList, error) {
 	path := fmt.Sprintf("/chart/%d/playlists", id)
 	result := PlaylistList{}
-	err := get(path, nil, &result)
+	err := get(path, listParams(index, limit), &result)
 	return result, err
 }
